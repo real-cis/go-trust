2fcbdd0c75aa48aa55766c8f57ba748adcf048ff
Sibi Antony <sibi.antony@real-cis.com>
2026-02-03 10:50:18 +0100

    compare cfv hash, tests with filtered eventlogs

--- ccel/eventlog_test.go
+++ ccel/eventlog_test.go
@@ -18,6 +18,7 @@ const (
 	EFIKEKHash        = "ade13d7b6cbd3bee3c6571c9d090550b83ecb8435c11bc6565f42ce0f799124a75e26a33c37419c8c1fd36507193627b"
 	EFIDbHash         = "125e0a524c332f19251973dab2eec2a36bf35f1b6ce585bf57c0c956cae5424dc05d8a087b48b2bc5b563acb63c360a8"
 	EFIDbxHash        = "5f95c5051ade2e2314961e4011150fbe3315c0b78c93b27be3ac8e4df73930e6ad82aa50ac3591d292ce364b04c850c7"
+	CFVHash           = "cc7dbc6e48cd02d110c75a560eb35b6fba8e1665576fe2103dc1c78a9acc73fe386c2b1778c980e91e51f0af9e25569b"
 )
 
 func TestEventLog(t *testing.T) {
@@ -69,4 +70,44 @@ func TestEventLog(t *testing.T) {
 	rtmr0 := replayMap[0][tcg.AlgSHA384]
 	assert.Equal(t, expectedRTMR0, hex.EncodeToString(rtmr0))
 
+}
+
+func TestFilterByEventType(t *testing.T) {
+	elBin, err := os.ReadFile("testdata/CCEL.1")
+	assert.Nil(t, err)
+
+	el := NewEventLogger(elBin, nil, tcg.PCClientFormat)
+	e := el.Parse()
+	assert.Nil(t, e)
+
+	filteredEvents := el.FilterByEventType([]tcg.EventType{
+		tcg.EvEfiPlatformFirmwareBlob2,
+		tcg.EvEfiVariableDriverConfig,
+	})
+
+	for _, fel := range filteredEvents {
+		eventType := fel.GetEventType()
+		assert.Contains(t, []tcg.EventType{tcg.EvEfiPlatformFirmwareBlob2, tcg.EvEfiVariableDriverConfig}, eventType)
+
+		switch eventType {
+		case tcg.EvEfiPlatformFirmwareBlob2:
+			assert.Equal(t, CFVHash, hex.EncodeToString(fel.GetDigests()[0].Hash))
+		case tcg.EvEfiVariableDriverConfig:
+			uefiVar, err := uefi.NewUefiVariableDataFromBytes(fel.GetEvent())
+			assert.NoError(t, err)
+
+			switch uefiVar.Name.String() {
+			case "PK":
+				assert.Equal(t, EFIPKHash, hex.EncodeToString(fel.GetDigests()[0].Hash))
+			case "KEK":
+				assert.Equal(t, EFIKEKHash, hex.EncodeToString(fel.GetDigests()[0].Hash))
+			case "db":
+				assert.Equal(t, EFIDbHash, hex.EncodeToString(fel.GetDigests()[0].Hash))
+			case "dbx":
+				assert.Equal(t, EFIDbxHash, hex.EncodeToString(fel.GetDigests()[0].Hash))
+			case "SecureBoot":
+				assert.Equal(t, EFISecureBootHash, hex.EncodeToString(fel.GetDigests()[0].Hash))
+			}
+		}
+	}
 }
